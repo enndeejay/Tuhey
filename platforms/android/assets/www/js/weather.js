daf1fb47117d34473f49ef8b4febc7aada8590e6
enndeejay <enndeejay@hotmail.com>
2016-04-19 14:01:38 -0400

    add platforms
    -Android
    -iOS

---
+++ platforms/android/assets/www/js/weather.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $.simpleWeather({
+     woeid: '',
+     location: 'Muncie, IN',
+     unit: 'f',
+     success: function(weather) {
+      html = '<h2>'+weather.temp+'&deg;'+weather.units.temp+'</h2>';
+      html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
+      html += '<li class="currently">'+weather.currently+'</li>';
+      html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
+      
+      for(var i=0;i<weather.forecast.length;i++) {
+        html += '<p>'+weather.forecast[i].day+': '+weather.forecast[i].high+'</p>';
+      }
+  
+      $("#weather").html(html);
+    },
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+  });
+});
